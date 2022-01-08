334ca14c5495df25de6584c507ecccaa50dbc019
Victor Andersson <victor.andersson3@tuta.io>
2022-01-08 01:31:24 +0100

    Fixing a bash install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Installing nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+echo Trying to move neovim to /usr/bin/nvim. Requiring sudo privilages
+sudo mv nvim.appimage /usr/bin/nvim
+
+# Installing the script
+mkdir $HOME/.config/neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mv init.vim $HOME/.config/nvim
