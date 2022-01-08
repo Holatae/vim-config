0d45b7988293b82e1f2f87c8af06e747a91333e7
Victor Andersson <victor.andersson3@tuta.io>
2022-01-08 01:54:33 +0100

    Fixed typo in folder name

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo mv nvim.appimage /usr/bin/nvim
 
 # Installing the script
 mkdir $HOME/.config
-mkdir $HOME/.config/neovim
+mkdir $HOME/.config/nvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
